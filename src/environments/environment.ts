ef6e41c640f91e1cb734cacee32a56628f8bef33
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-05-16 14:39:13 +0530

    [Akshay]: Update to relative paths for universal compatibility (Local & EC2)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: '/api/v1',
-  oauthRedirectUrl: 'http://localhost:8080/api/v1/oauth2/authorization/google',
-  githubOauthRedirectUrl: 'http://localhost:8080/api/v1/oauth2/authorization/github',
+  oauthRedirectUrl: '/api/v1/oauth2/authorization/google',
+  githubOauthRedirectUrl: '/api/v1/oauth2/authorization/github',
   razorpayKeyId: 'rzp_test_SlYX9Y9q8Vr2aA'
-};
+};
